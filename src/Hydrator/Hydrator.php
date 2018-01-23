2b7dfa0f669f7dfe8c7573a463042b044a823bfe
Sebastian Rogala <sebrogala@gmail.com>
2018-01-23 14:20:21 +0100

    Add ZendHydrator as requirement and proxy Hydrator

---
+++ src/Hydrator/Hydrator.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Author: Sebastian Rogala
+ * Mail: sebrogala@gmail.com
+ * Created: 23.01.18
+ */
+
+namespace Xsv\Doctrine\Hydrator;
+
+/**
+ * Class Hydrator
+ *
+ * Created to be as proxy to original DoctrineObject.
+ * Reason is to keep intuitive naming.
+ *
+ * @package Xsv\Doctrine\Hydrator
+ */
+class Hydrator extends DoctrineObject
+{
+
+}
